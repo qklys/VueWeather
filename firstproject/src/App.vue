de30fa5bab52523eb61f7aa335f5f0493cbae1d6
Сергей Филатов <modern443@gmail.com>
2020-12-10 18:08:03 +0300

    todo

---
+++ firstproject/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <TodoList v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/ToDoList.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      todos: ['Зарядка', 'Завтрак', 'Завтрак', 'Завтрак']
+    }
+  },
+  components: {
+    TodoList
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
